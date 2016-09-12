1c10c11d1eb8d52af166c42c8b5b50c34a5f5224
Philipp Melab <philipp.melab@gmail.com>
2016-09-12 07:02:25 +0200

    Don't update during rsync.

--- sync.sh
+++ sync.sh
@@ -3,4 +3,4 @@ printf "" >> /exclude.txt
 for ex in $EXCLUDE; do
   printf "%s\n" "$ex" >> /exclude.txt
 done
-rsync -qrtvu --delete --exclude-from /exclude.txt "${SOURCE:-/source}/" "${DESTINATION:-/destination}/"
+rsync -qrtv --delete --exclude-from /exclude.txt "${SOURCE:-/source}/" "${DESTINATION:-/destination}/"
