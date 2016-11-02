20c54ed92d8b152162242995e192da8c0386bf8f
Philipp Melab <philipp.melab@gmail.com>
2016-11-02 11:18:30 +0100

    Switching to --whole-file mode for local transfers.

--- sync.sh
+++ sync.sh
@@ -3,4 +3,4 @@ printf "" >> /exclude.txt
 for ex in $EXCLUDE; do
   printf "%s\n" "$ex" >> /exclude.txt
 done
-rsync -qrtv --delete --exclude-from /exclude.txt "${SOURCE:-/source}/" "${DESTINATION:-/destination}/"
+rsync -Wqrtv --delete --exclude-from /exclude.txt "${SOURCE:-/source}/" "${DESTINATION:-/destination}/"
