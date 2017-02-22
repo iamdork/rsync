8e2abf52158dd508c317ca843b2005fd5a0159d5
Philipp Melab <philipp.melab@gmail.com>
2017-02-22 17:48:03 +0100

    Added symlink option.

--- sync.sh
+++ sync.sh
@@ -3,4 +3,4 @@ printf "" >> /exclude.txt
 for ex in $EXCLUDE; do
   printf "%s\n" "$ex" >> /exclude.txt
 done
-rsync -Wqrtv --delete --exclude-from /exclude.txt "${SOURCE:-/source}/" "${DESTINATION:-/destination}/"
+rsync -Wqrtvl --delete --exclude-from /exclude.txt "${SOURCE:-/source}/" "${DESTINATION:-/destination}/"
